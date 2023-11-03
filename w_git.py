fee48c408a7af230cd8575665ce8f42f035088b0
wendel <wendelhector33@gmail.com>
2023-11-02 21:53:33 -0400

    First commit

---
+++ w_git.py
@@ -0,0 +1,18 @@
+print("Wendel Hector\n\n\n")
+import random
+#Premye pati
+# nimero 1
+chen_karakte = input("Antre yon fraz avek majiskil: ")
+print("Chen nan an miniskil net:"+chen_karakte.lower())
+
+#nimero 2
+chen = input("Antre yon chen karakte: ")
+b = chen.split()
+print(b)
+
+#nimero 3
+chen_ti = input("Antre yon chen: ")
+d = chen_ti.title()
+print(d)
+
+print("nap travay san kanpe, se fristal nap fristal")
